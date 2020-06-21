2b8b60bd43bf8ddfc41140f077b32764bf7f03af
Cody C <codyicastro@gmail.com>
2020-06-21 17:17:45 +0000

    Automatically backed up by Learn

--- lib/example.rb
+++ lib/example.rb
@@ -1,6 +1,11 @@
 # Use this space to code along with the readme
-phrase = "Hello World!"
 
-10.times do 
-  puts phrase
+
+def say_hello_world_ten_times
+ phrase = "Hello World!"
+  10.times do 
+   puts phrase
+  end
 end
+
+
